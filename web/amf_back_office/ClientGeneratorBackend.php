e24f86e05ea0b7c1456db6fb47a8cd1a30629dd8
Costas Vassilakis <costas@uop.gr>
2024-12-07 18:48:58 +0200

    Update ClientGeneratorBackend.php
    removed parentheses from require_once

--- web/amf_back_office/ClientGeneratorBackend.php
+++ web/amf_back_office/ClientGeneratorBackend.php
@@ -11,7 +11,7 @@
 /**
  * includes
  */
-require_once(dirname(__FILE__) . '/ClassLoader.php');
+require_once dirname(__FILE__) . '/ClassLoader.php';
 $accessManager = new Amfphp_BackOffice_AccessManager();
 $isAccessGranted = $accessManager->isAccessGranted();
 if(!$isAccessGranted){
